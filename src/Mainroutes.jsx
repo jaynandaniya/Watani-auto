543e1614a87d04991c531b8683180b6d43875807
jaynandaniya <jaynandaniya2567@gmail.com>
2023-09-28 14:24:14 +0530

    first commit

---
+++ src/Mainroutes.jsx
@@ -0,0 +1,11 @@
+import './Mainroutes.css';
+import Loginbck from './component/header/Loginbck';
+function Mainroutes() {
+    return (
+      <div >
+       <Loginbck />
+      </div>
+    );
+  }
+  
+  export default Mainroutes;
---
+++ src/component/header/Loginbck.jsx
@@ -0,0 +1,13 @@
+
+function Loginbck() {
+    return (
+      <div>
+          <div className="loginbck">
+          <div className="j">
+          edfv</div>
+          </div>
+      </div>
+    );
+  }
+  
+  export default Loginbck;
