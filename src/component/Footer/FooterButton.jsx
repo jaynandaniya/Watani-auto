b606856d33993ad69ba2f5fca9fca89ed53d208d
jaynandaniya <jaynandaniya2567@gmail.com>
2023-10-02 17:04:38 +0530

    message

---
+++ src/component/Footer/FooterButton.jsx
@@ -0,0 +1,20 @@
+import '../header/Loginform'
+function FooterButton() {
+    return (
+      <div>
+          <div className="d-flex FooterButtom">
+            <ul>
+              <div><li><a href="#">About Us</a></li></div>
+              <div><li><a href="#">About Us</a></li></div>
+              
+              <li><a href="#">Terms and Conditions</a></li>
+              <li><a href="#">Privacy policy</a></li>
+              <li><a href="#">Help</a></li>
+              <li><a href="#">Careers</a></li>
+              <li><a href="#">Cancellation policy</a></li>
+            </ul>
+          </div>
+      </div>
+    );
+  }
+  export default FooterButton;
--- src/component/header/Loginform.jsx
+++ src/component/header/Loginform.jsx
@@ -1,34 +1,36 @@
-
+import FooterButton from '../Footer/FooterButton'
 function Loginform() {
     return (
-
-      <div>
-           <div className="container d-flex blur">
-       
-                {/* <div></div> */}
-                <div><h2 className="text-right">Welcome !
-        </h2>
-        <form>
-            <div className="mb-3">                
+        <div>
+      <div className=" container d-flex gap "> 
+            <div className="coll-70"><span></span></div>
+            <div className="coll-30 blur">
+                     
+               <form className="mb-3">
+               <h2 className="text-right py-5 mb-3  ">Welcome !</h2>
+               <div className="mb-3">
+            <div className="mb-4">                
                 <input type="email" className="login-input" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
             </div>
-            <div className="mb-3 ">
+            <div className="mb-4">
                 <input type="password" className="login-input" id="password" placeholder="Password" />
             </div>
-            <div className=" text-left "> 
-            {/* d-flex flex-row-reverse bd-highlight */}
-             <a href="#"> forget Password?</a>
+            <div className="forgett-psw text-end"> 
+             <a href="#" className="text-end"> forget Password?</a>
             </div>
-            <div className="text-center mb-3 ">
-                <a type="submit" className="center login-btn">Login</a>
+            <div className="">
+                <a type="submit" className="text-center login-btn">Login</a>
             </div>
-            <div className="text-center  d-flex mt-1 ">
-              
+            <div className=" user-register gap-4 py-5 mb-3">
+                <a href="#" className="text-center">New User?</a>
+                <a href="#" className="text-center ">Register Now</a>
             </div>
-        </form></div>
-        
-    </div>     
-          </div>
+            </div>
+                  </form>
+             </div>     
+        </div>
+            <FooterButton />
+        </div>
     );
   }
   
